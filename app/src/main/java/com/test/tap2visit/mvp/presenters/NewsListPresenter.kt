d4ca32d98c56bfaf273471d874540f93f9ea565f
schnaps1981 <schnaps1981@gmail.com>
2020-06-11 16:16:50 +0700

    унес домой

--- app/src/main/java/com/test/tap2visit/mvp/presenters/NewsListPresenter.kt
+++ app/src/main/java/com/test/tap2visit/mvp/presenters/NewsListPresenter.kt
@@ -1,7 +1,9 @@
 package com.test.tap2visit.mvp.presenters
 
 import com.test.tap2visit.app.App
+import com.test.tap2visit.global.Paginator
 import com.test.tap2visit.mvp.views.NewsListView
+import com.test.tap2visit.network.api.entity.Article
 import moxy.InjectViewState
 import moxy.MvpPresenter
 import ru.terrakok.cicerone.Router
@@ -12,8 +14,11 @@ class NewsListPresenter : MvpPresenter<NewsListView>() {
     @Inject
     lateinit var router: Router
 
+    lateinit var paginator: Paginator<Article>
+
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         App.instance.inject(this)
+
     }
 }
